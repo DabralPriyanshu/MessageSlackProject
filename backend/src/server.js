571110cb5af6a3d0708a288f8aaa213c88653d97
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-03-08 20:01:04 +0530

    Added body parsing middlewares

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import ENV from "./config/serverConfig.js";
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/ping", (req, res) => {
   res.status(200).json({ message: "pong" });
 });
