5c3332ecb01253e2811dc052e48a1f9d11e1cfd4
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-03-11 21:38:17 +0530

    Setup tailwind and shadcnui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Button } from "./components/ui/button";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button>Default Button</Button>
+      <Button variant="destructive">Delete</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+    </div>
+  );
 };
 
 export default App;
